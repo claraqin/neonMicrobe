436a1f358acd5d6eda1b78699b131eb90e1d72bb
C. Qin <kennethqin15@gmail.com>
2019-08-09 16:51:54 -0700

    fixed download filename

--- code/downloadRawSequenceData.R
+++ code/downloadRawSequenceData.R
@@ -1,6 +1,13 @@
 ## Function downloads the metadata for various sequencing data products and downloads the ##
 ## raw data files ##
 
+# sites = "HARV"
+# startYrMo = "2014-06"
+# endYrMo = "2014-07"
+# outdir = "/afs/cats.ucsc.edu/users/b/claraqin/zhulab/NEON_DoB_analysis/data/Illumina"
+# # outdir = "~/Downloads"
+# checkFileSize = TRUE
+
 downloadRawSequenceData <- function(sites="all", startYrMo="YYYY-MM", endYrMo="YYYY-MM", 
                                     outdir="~/Downloads", checkFileSize=TRUE) {
   # outdir - path to directory to ouput the data. Defaults to the R default directory if none provided
@@ -18,12 +25,12 @@ downloadRawSequenceData <- function(sites="all", startYrMo="YYYY-MM", endYrMo="Y
   
   for(i in 1:length(u.urls)) {
     download.file(url=as.character(u.urls[i]), destfile = ifelse(dir.exists(outdir), 
-                                              paste(outdir, fileNms, sep="/"), 
-                                             paste(getwd(), fileNms, sep="/" )) ) 
+                                              paste(outdir, fileNms[i], sep="/"), 
+                                             paste(getwd(), fileNms[i], sep="/" )) ) 
     if(dir.exists(outdir)) {
-      print(paste("Finished downloading", paste(outdir, fileNms, sep="/")))
+      print(paste("Finished downloading", paste(outdir, fileNms[i], sep="/")))
     } else {
-      print(paste("Finished downloading", paste(getwd(), fileNms, sep="/" )))
+      print(paste("Finished downloading", paste(getwd(), fileNms[i], sep="/" )))
     }
   }
   
