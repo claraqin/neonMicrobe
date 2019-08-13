59f4005cf5cd4c6232c45f84f1e3cbd7336ffbf1
claraqin <claraqin@socs-stats.ucsc.edu>
2019-08-13 15:29:47 -0700

    continuing dada2 tutorial

--- code/dada2_tutorial.R
+++ code/dada2_tutorial.R
@@ -1,6 +1,11 @@
+# DADA2 tutorial
+# Follows https://benjjneb.github.io/dada2/tutorial.html
+
+# May want to follow this too https://benjjneb.github.io/dada2/ITS_workflow.html
+
 library(dada2)
 
-path <- "~/Downloads/R1"
+path <- "~/zhulab/NEON_DoB_analysis/data/Illumina"
 list.files(path)
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
@@ -9,4 +14,11 @@ fnFs <- sort(list.files(path, pattern="_R1.fastq", full.names = FALSE)) # If set
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
 sample.names <- sub("_R1.fastq", "", fnFs)
 
-plotQualityProfile(fnFs[1:2])
+plotQualityProfile(paste(path, fnFs[1:2], sep="/"))
+
+# Place filtered files in filtered/ subdirectory
+filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
+filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
+names(filtFs) <- sample.names
+names(filtRs) <- sample.names
+
