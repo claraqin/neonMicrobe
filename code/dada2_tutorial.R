d05128b2eac971fdd8f4f6a788bf6158578a9b7b
claraqin <claraqin@socs-stats.ucsc.edu>
2019-08-16 18:17:03 -0700

    halfway through ITS tutorial; finished filtN

--- code/dada2_tutorial.R
+++ code/dada2_tutorial.R
@@ -2,13 +2,89 @@
 # Follows the ITS workflow https://benjjneb.github.io/dada2/ITS_workflow.html
 
 library(dada2)
+library(ShortRead)
+library(Biostrings)
 
-path <- "~/zhulab/NEON_DoB_analysis/data/Illumina/ITS"
+path <- "/afs/cats.ucsc.edu/users/b/claraqin/zhulab/NEON_DoB_analysis/data/Illumina/ITS"
 list.files(path)
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
-fnFs <- sort(list.files(path, pattern="_R1.fastq", full.names = FALSE)) # If set to be FALSE, then working directory must contain the files
-# fnRs <- sort(list.files(path, pattern="_R2.fastq", full.names = FALSE)) # none for now
+fnFs <- sort(list.files(path, pattern="_R1.fastq", full.names = TRUE)) # If set to be FALSE, then working directory must contain the files
+fnRs <- sort(list.files(path, pattern="_R2.fastq", full.names = TRUE))
+
+# Remove any forward files that don't have reverse counterparts, and vise versa
+# (filterAndTrim will throw an error if fnFs and fnRs have any mismatches)
+basefilenames_Fs <- sub("_R1.fastq","",basename(fnFs))
+basefilenames_Rs <- sub("_R2.fastq","",basename(fnRs))
+rm_from_fnFs <- basefilenames_Fs[which(!(basefilenames_Fs %in% basefilenames_Rs))]
+rm_from_fnRs <- basefilenames_Rs[which(!(basefilenames_Rs %in% basefilenames_Fs))]
+# During this tutorial, I found that "BMI_Plate60WellG10_ITS" exists in the reverse-read
+# files but not in the forward-read files. I'll remove this.
+
+for(name in rm_from_fnFs) {
+  print(paste(name, "does not have a reverse-reads counterpart. Omitting from this analysis."))
+  fnFs <- fnFs[-which(fnFs == paste0(path, "/", name, "_R1.fastq"))]
+}
+for(name in rm_from_fnRs) {
+  print(paste(name, "does not have a forward-reads counterpart. Omitting from this analysis."))
+  fnRs <- fnRs[-which(fnRs == paste0(path, "/", name, "_R2.fastq"))]
+}
+
+
+# Identify primers
+FWD <- "CTTGGTCATTTAGAGGAAGTAA"  ## CHANGE ME to your forward primer sequence
+REV <- "GCTGCGTTCTTCATCGATGC"  ## CHANGE ME...
+
+# Get all orientations of primers, just to be safe
+allOrients <- function(primer) {
+  # Create all orientations of the input sequence
+  require(Biostrings)
+  dna <- DNAString(primer)  # The Biostrings works w/ DNAString objects rather than character vectors
+  orients <- c(Forward = dna, Complement = complement(dna), Reverse = reverse(dna), 
+               RevComp = reverseComplement(dna))
+  return(sapply(orients, toString))  # Convert back to character vector
+}
+FWD.orients <- allOrients(FWD)
+REV.orients <- allOrients(REV)
+FWD.orients
+
+# “pre-filter” the sequences just to remove those with Ns, but perform no other filtering
+fnFs.filtN <- file.path(path, "filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
+fnRs.filtN <- file.path(path, "filtN", basename(fnRs))
+filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE)
+# "Mismatched forward and reverse sequence files" error occurs many times:
+# One of these is at i=1261
+# I believe this happens whenever at least one file in the forward-reverse pair
+# does not pass the "N" filter.
+
+# Only 4392 out of 11049 ITS fastq files passed?
+
+
+# (From tutorial) We are now ready to count the number of times the primers appear in the 
+# forward and reverse read, while considering all possible primer orientations. 
+# Identifying and counting the primers on one set of paired end FASTQ files is
+# sufficient, assuming all the files were created using the same library preparation,
+# so we’ll just process the first sample.
+primerHits <- function(primer, fn) {
+  # Counts number of reads in which the primer is found
+  nhits <- vcountPattern(primer, sread(readFastq(fn)), fixed = FALSE)
+  return(sum(nhits > 0))
+}
+rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]), 
+      FWD.ReverseReads = sapply(FWD.orients, primerHits, fn = fnRs.filtN[[1]]), 
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]), 
+      REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.filtN[[1]]))
+
+
+###
+
+
+
+
+
+
+
+
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
 sample.names <- sub("_R1.fastq", "", fnFs)
 
