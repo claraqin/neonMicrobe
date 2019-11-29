992906d2baf88fa4801c0f1c9167b4dcfcbd287e
C. Qin <kennethqin15@gmail.com>
2019-11-29 11:53:52 -0800

    moved params from utils.R to params.R

--- code/utils.R
+++ code/utils.R
@@ -4,16 +4,8 @@ library(neonUtilities)
 library(utils)
 library(dplyr)
 
-# Preset parameters for the "All" variants of download functions:
-preset_sites = "all"
-preset_startYrMo = "2011-06"
-preset_endYrMo = format(Sys.Date(), "%Y-%m")
-preset_outdir_sequence = "/data/ZHULAB/NEON_DOB/Illumina/NEON"
-preset_outdir_seqmeta = "/data/ZHULAB/NEON_DOB/sequence_metadata"
-preset_outdir_soil = "/data/ZHULAB/NEON_DOB/soil"
-preset_checkFileSize = FALSE
-preset_return_data = TRUE
-site_and_date_range_filename = "sites_and_date_range.txt"
+# Load parameters from params.R
+source("./code/params.R")
 
 ## Function to write site and date range parameters. To be called within other functions.
 #
