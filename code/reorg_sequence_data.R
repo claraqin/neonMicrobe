cf0b5ffac9173356a8e258b10e30ee430fada755
claraqin <claraqin@socs-stats.ucsc.edu>
2020-02-04 09:31:00 -0800

    reorg now splits data into ITS/16S subdirs

--- code/reorg_sequence_data.R
+++ code/reorg_sequence_data.R
@@ -3,11 +3,13 @@
 # This script automates the instructions in the "New server setup"
 # page in the GitHub Wiki, EXCEPT that it only downloads ITS data
 # (so far) and it does not perform the operations in the 
-# "Pre-processing" section. It also does not split files into
-# ITS and 16S subdirectories.
+# "Pre-processing" section. 
+# This script now splits files into ITS and 16S subdirectories.
 # Furthermore, this script appends the sequencing run ID to all
 # fastq filenames, so they can be processed by DADA2 in batches
 # corresponding to sequencing runs.
+
+source("./code/params.R")
 
 BASE_DIR <- preset_outdir_sequence
 
@@ -33,10 +35,12 @@ for(i in 1:length(zipF[2:6])) { # <-------------------------- TODO: CHANGE TO BE
   
   # list all unzipped files
   # This regex matches all files with basenames that do NOT begin with "run" and which end with ".fastq"
-  unzippedF <- grep("/(?!run)[^/]*fastq$", list.files(path = BASE_DIR, recursive=TRUE, full.names=TRUE), perl=TRUE, value=TRUE)
+  unzippedF_ITS <- grep("/(?!run)[^/]*_ITS[^/]*fastq$", list.files(path = BASE_DIR, recursive=TRUE, full.names=TRUE), perl=TRUE, value=TRUE)
+  unzippedF_16S <- grep("/(?!run)[^/]*_16S[^/]*fastq$", list.files(path = BASE_DIR, recursive=TRUE, full.names=TRUE), perl=TRUE, value=TRUE)
   
   # rename all unzipped files by appending sequencer run ID
-  unzippedF_rename <- file.path(BASE_DIR, paste0("run", runID, "_", basename(unzippedF)))
+  unzippedF_ITS_rename <- file.path(BASE_DIR, "ITS", paste0("run", runID, "_", basename(unzippedF_ITS)))
+  unzippedF_16S_rename <- file.path(BASE_DIR, "16S", paste0("run", runID, "_", basename(unzippedF_16S)))
   file.rename(unzippedF, unzippedF_rename)
 }
 
