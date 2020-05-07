f004bdff57add28219a3db17f0339317eba7ab18
claraqin <claralqin@gmail.com>
2020-05-07 11:46:18 -0700

    added tax testing and plots

--- code/test_dada2_params.R
+++ code/test_dada2_params.R
@@ -6,7 +6,7 @@
 # TO RUN 00_new_server_setup.R BEFORE RUNNING THIS SCRIPT. IN
 # ADDITION, YOU WILL NEED TO DECOMPRESS THE 'C25G9_sample.zip' FILE,
 # AVAILABLE AT THE LINK BELOW, AND PASTE ITS CONTENTS INTO 'PATH_CUT',
-# DEFINED ON LINE 61.
+# DEFINED ON LINE ~57.
 #
 # LINK TO SAMPLE FILES: https://drive.google.com/file/d/1sIyp3_Ne1vF7qLf-racqoyNbEL_1qZGL/view?usp=sharing
 #
@@ -27,6 +27,7 @@ library(Biostrings)
 library(tibble)
 library(dplyr)
 library(vegan)
+library(phyloseq)
 
 ### TEMPORARY ############
 # Create required directories
@@ -107,17 +108,17 @@ filtFs <- lapply(param_sets, function(x) file.path(PATH_TEST, x, basename(cutFs)
 filtRs <- lapply(param_sets, function(x) file.path(PATH_TEST, x, basename(cutRs)))
 
 # Run filterAndTrim on samples with all 25 parameter sets
-list_out <- list()
+out_list <- list()
 for (i in 1:nrow(params)) {
   out <- filterAndTrim(
     fwd = cutFs, filt = filtFs[[i]], rev = cutRs, filt.rev = filtRs[[i]], 
     compress = TRUE, multithread = TRUE, maxN = 0,
     maxEE = params[i,1:2], truncQ = params[i,3], minLen = params[i,4]
   )
-  list_out[[i]] <- as.data.frame(out) %>% mutate(prop.out = reads.out / reads.in)
+  out_list[[i]] <- as.data.frame(out) %>% mutate(prop.out = reads.out / reads.in)
 }
 
-names(list_out) <- param_sets
+names(out_list) <- param_sets
 
 # Summarize and plot number/proportion of reads remaining
 library(ggplot2)
@@ -126,7 +127,7 @@ library(ggplot2)
 data.frame(
   maxEE = rep(params[,1], each=5),
   truncQ = rep(params[,3], each=5),
-  reads.out = unlist(lapply(list_out, function(x) x[,"reads.out"]))
+  reads.out = unlist(lapply(out_list, function(x) x[,"reads.out"]))
 ) %>%
   ggplot(aes(x=maxEE, y=truncQ, col=reads.out)) +
   geom_jitter() +
@@ -136,7 +137,7 @@ data.frame(
 data.frame(
   maxEE = params[,1],
   truncQ = params[,3],
-  reads.out = unlist(lapply(list_out, function(x) median(x[,"reads.out"])))
+  reads.out = unlist(lapply(out_list, function(x) median(x[,"reads.out"])))
 ) %>%
   ggplot(aes(x=maxEE, y=truncQ, col=reads.out)) +
   geom_point() +
@@ -146,7 +147,7 @@ data.frame(
 data.frame(
   maxEE = params[,1],
   truncQ = params[,3],
-  prop.out = unlist(lapply(list_out, function(x) median(x[,"prop.out"])))
+  prop.out = unlist(lapply(out_list, function(x) median(x[,"prop.out"])))
 ) %>%
   ggplot(aes(x=maxEE, y=truncQ, col=prop.out)) +
   geom_point() +
@@ -154,6 +155,18 @@ data.frame(
   ggtitle("Median proportion of reads remaining\nafter filterAndTrim") +
   xlab("maxEE: after truncation, max allowable expected errors") +
   ylab("truncQ: quality score at which point to truncate read")
+# Alternate version
+data.frame(
+  maxEE = params[,1],
+  truncQ = params[,3],
+  prop.out = unlist(lapply(out_list, function(x) median(x[,"prop.out"])))
+) %>%
+  ggplot(aes(x=truncQ, y=prop.out, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Proportion of reads remaining\nafter filterAndTrim") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Prop. reads remaining (median of run)")
 
 # These plots don't tell us much, because there is no guarantee that 
 # any of these reads are of sufficient quality to be assigned taxonomy,
@@ -170,6 +183,9 @@ filtRs <- lapply(filtRs, function(x) { x <- x[file.exists(x)] })
 n_merged <- list()
 prop_merged <- list()
 seqtabs <- list()
+dadaFs_list <- list()
+dadaRs_list <- list()
+system.time({
 for(i in 1:length(filtFs)) {
   filtFs.star <- filtFs[[i]]
   filtRs.star <- filtRs[[i]]
@@ -200,6 +216,8 @@ for(i in 1:length(filtFs)) {
   ## TODO: Record intermediate metric here: the number of
   ##       sequence variants partitioned from the full set
   ##       of reads after the denoising algorithm.
+  dadaFs_list[[i]] <- dadaFs
+  dadaRs_list[[i]] <- dadaRs
   
   # Merge pairs
   mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE, returnRejects=TRUE)
@@ -231,59 +249,104 @@ for(i in 1:length(filtFs)) {
   
   seqtabs[[i]] <- seqtab.nochim
 }
+})
 names(n_merged) <- param_sets
 names(prop_merged) <- param_sets
+names(dadaFs_list) <- param_sets
+names(dadaRs_list) <- param_sets
 names(seqtabs) <- param_sets
+
+# Save the lists
+saveRDS(dadaFs_list, "./data/sensitivity_dadaFs_list.Rds")
+saveRDS(dadaRs_list, "./data/sensitivity_dadaRs_list.Rds")
+saveRDS(seqtabs, "./data/sensitivity_seqtabs_list.Rds")
 
 n_merged # This is the number of ASVs (as partitioned by dada) that successfully merged
 prop_merged # This is proportion of ASVs (as partitioned by dada) that successfully merged
+# The below is the proportion of forward reads that were assigned an ASV
+prop_Fs_mapped_to_asv <- lapply(dadaFs_list, function(x) lapply(x, function(y) mean(!is.na(y$map))))
+prop_Fs_mapped_to_asv_mat <- matrix(
+  unlist(prop_Fs_mapped_to_asv),
+  ncol=5,nrow=25,dimnames=list(param_sets, basename(cutFs))
+)
+head(prop_Fs_mapped_to_asv_mat)
 
 # Rate of successfully merged ASVs per pre-filterAndTrim read
 # (Not sure if this is meaningful -- treats denoising algorithm as a black box)
-merged_variants_per_input_read <- lapply(n_merged, function(x) x/list_out[[1]][,"reads.in"])
+merged_variants_per_input_read <- lapply(n_merged, function(x) x/out_list[[1]][,"reads.in"])
 merged_variants_per_input_read
 
-# Summarize and plot the proportion of ASVs that successfully merged
-data.frame(
+# Summarize
+summary_df <- 
+  data.frame(
   maxEE = params[,1],
   truncQ = params[,3],
-  prop.merged = unlist(lapply(prop_merged, function(x) median(x)))
-) %>%
+  prop.Fs.mapped = unlist(lapply(prop_Fs_mapped_to_asv, function(x) median(unlist(x)))),
+  prop.merged = unlist(lapply(prop_merged, function(x) median(x))),
+  rate.merged = unlist(lapply(merged_variants_per_input_read, function(x) median(x)))
+)
+
+# Plot the proportion of forward reads that were assigned to an ASV
+summary_df %>%
+  ggplot(aes(x=maxEE, y=truncQ, col=prop.Fs.mapped)) +
+  geom_point() +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Proportion of (forward) reads assigned to an ASV") +
+  xlab("maxEE: after truncation, max allowable expected errors") +
+  ylab("truncQ: quality score at which point to truncate read")
+# Alternate version
+summary_df %>%
+  ggplot(aes(x=truncQ, y=prop.Fs.mapped, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Proportion of reads assigned to an ASV") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Prop. (forward) reads assigned to ASV\n(median of run)")
+
+# Plot the proportion of ASVs that successfully merged
+summary_df %>%
   ggplot(aes(x=maxEE, y=truncQ, col=prop.merged)) +
   geom_point() +
   scale_colour_gradient(low="blue", high="red") +
-  ggtitle("Median proportion of ASVs successfully merged") +
+  ggtitle("Proportion of ASVs that successfully merged") +
   xlab("maxEE: after truncation, max allowable expected errors") +
   ylab("truncQ: quality score at which point to truncate read")
+# Alternate version
+summary_df %>%
+  ggplot(aes(x=truncQ, y=prop.merged, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Proportion of ASVs that successfully merged") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Rate of successful merges (median of run)")
 
-# Summarize and plot the rate of merged ASVs per pre-filter read
+# Plot the rate of merged ASVs per pre-filter read
 # (Not sure if this is meaningful -- treats denoising algorithm as a black box)
-data.frame(
-  maxEE = params[,1],
-  truncQ = params[,3],
-  rate.merged = unlist(lapply(merged_variants_per_input_read, function(x) median(x)))
-) %>%
+summary_df %>%
   ggplot(aes(x=maxEE, y=truncQ, col=rate.merged)) +
   geom_point() +
   scale_colour_gradient(low="blue", high="red") +
-  ggtitle("Median rate of successfully merged ASVs\nper input read") +
+  ggtitle("Successfully merged ASVs\nper input read") +
   xlab("maxEE: after truncation, max allowable expected errors") +
   ylab("truncQ: quality score at which point to truncate read")
+# Alternate version
+# summary_df %>%
+#   ggplot(aes(x=maxEE, y=rate.merged)) +
+#   geom_point() +
+#   ggtitle("Successfully merged ASVs\nper input read") +
+#   xlab("maxEE: after truncation, max allowable expected errors") +
+#   ylab("Successfully merged ASVs per input read\n(median of run)")
+summary_df %>%
+  ggplot(aes(x=truncQ, y=rate.merged, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Successfully merged ASVs\nper input read") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Successfully merged ASVs per input read\n(median of run)")
 
 # These results indicate that the best parameters for ensuring
 # a high number of successfully merged ASVs are low values
 # for truncQ (2-8) and any values for maxEE (2-32)
-
-##################
-# What's the best way to evaluate effects on diversity estimates?
-# Do the parameters affect the shape of the rarefaction curves,
-# or just the sequencing depth?
-par(mfrow=c(2,2))
-lapply(seqtabs, function(x) {
-  rarecurve(x, step=50, label=FALSE)
-})
-par(mfrow=c(1,1))
-rarecurve(seqtabs[[4]], sample=50)
 
 # Assign taxonomy using the UNITE database
 unite.ref <- "./raw_data/tax_ref/sh_general_release_dynamic_02.02.2019.fasta"
@@ -297,11 +360,128 @@ taxas.print <- taxas[[1]]
 rownames(taxas.print) <- NULL
 head(taxas.print)
 
+saveRDS(taxas, "./data/sensitivity_taxas.Rds")
+
+
+###################
+# Evaluate effects on taxonomic resolution.
 # What's the best way to evaluate taxonomic resolution?
-# Number of non-missing values in the Species column? Not sure
-for(i in 1:length(param_sets)) {
-  print(paste0("Parameter set: ", param_sets[i]))
-  print(paste0("- Nonmissing species-level classifications: ", sum(!is.na(taxas[[i]][,"Species"]))))
-  print(paste0("- Total number of ASVs: ", nrow(taxas[[i]])))
+# Number of taxa without a species-level assignment?
+physeqs <- list()
+abund_spp_classification <- list() # Total abundance of ASVs with species-level classifications
+# prop_spp_classification <- list() # Proportion-by-abundance of ASVs with species-level classification
+n_spp_classification <- list() # Number of unique ASVs with species-level classification
+for(i in 1:length(seqtabs)) {
+  physeqs[[i]] <- phyloseq(otu_table(seqtabs[[i]],taxa_are_rows=FALSE), tax_table(taxas[[i]]))
+  abund_spp_classification[[i]] <- sum(otu_table(subset_taxa(physeqs[[i]], !is.na(Species))))
+  # prop_spp_classification[[i]] <- abund_spp_classification[[i]] / sum(otu_table(physeqs[[i]]))
+  n_spp_classification[[i]] <- sum(!is.na(tax_table(physeqs[[i]])[,"Species"]))
+}
+abund_spp_classification
+# prop_spp_classification
+n_spp_classification
+
+# Remake summary_df with additional columns
+summary_df <- 
+  data.frame(
+    maxEE = params[,1],
+    truncQ = params[,3],
+    prop.Fs.mapped = unlist(lapply(prop_Fs_mapped_to_asv, function(x) median(unlist(x)))),
+    prop.merged = unlist(lapply(prop_merged, function(x) median(x))),
+    rate.merged = unlist(lapply(merged_variants_per_input_read, function(x) median(x))),
+    abund.spp.classification = unlist(abund_spp_classification),
+    n.spp.classification = unlist(n_spp_classification)
+  )
+
+# Plot the abundance of species-level classifications in each physeq
+# (Not sure if this is meaningful -- treats everything in between as black box)
+summary_df %>%
+  ggplot(aes(x=maxEE, y=truncQ, col=abund.spp.classification)) +
+  geom_point() +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Abundance of ASVs with species-level\nclassifications") +
+  xlab("maxEE: after truncation, max allowable expected errors") +
+  ylab("truncQ: quality score at which point to truncate read")
+
+# Two-dim versions
+summary_df %>%
+  ggplot(aes(x=maxEE, y=abund.spp.classification)) +
+  geom_point(alpha=0.6) +
+  ggtitle("Abundance of ASVs with species-level\nclassifications") +
+  xlab("maxEE: after truncation, max allowable expected errors") +
+  ylab("Total abundance of spp-level ASVs")
+summary_df %>%
+  ggplot(aes(x=truncQ, y=abund.spp.classification, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Abundance of ASVs with species-level\nclassifications") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Total abundance of spp-level ASVs")
+
+# Plot the number of unique ASVs with species-level classification
+summary_df %>%
+  ggplot(aes(x=maxEE, y=truncQ, col=n.spp.classification)) +
+  geom_point() +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Number of unique ASVs with\nspecies-level classifications") +
+  xlab("maxEE: after truncation, max allowable expected errors") +
+  ylab("truncQ: quality score at which point to truncate read")
+
+# Two-dim versions
+summary_df %>%
+  ggplot(aes(x=maxEE, y=n.spp.classification)) +
+  geom_point(alpha=0.6) +
+  ggtitle("Number of unique ASVs with\nspecies-level classifications") +
+  xlab("maxEE: after truncation, max allowable expected errors") +
+  ylab("Total abundance of spp-level ASVs")
+summary_df %>%
+  ggplot(aes(x=truncQ, y=n.spp.classification, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Number of unique ASVs with\nspecies-level classifications") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("No. unique spp-level ASVs")
+
+##################
+# What's the best way to evaluate effects on diversity estimates?
+# Do the parameters affect the shape of the rarefaction curves,
+# or just the sequencing depth?
+par(mfrow=c(2,2))
+lapply(seqtabs, function(x) {
+  rarecurve(x, step=50, label=FALSE)
+})
+par(mfrow=c(1,1))
+rarecurve(seqtabs[[4]], sample=50)
+
+# Use phyloseq's estimate_richness function?
+?estimate_richness
+estimate_richness(physeqs[[1]])
+# Ben Callahan recommends not using diversity estimates that rely on singletons.
+# This leaves Shannon and Simpson's indices. https://github.com/benjjneb/dada2/issues/214
+estimate_richness(physeqs[[1]], measures=c("Shannon"))
+estimate_richness(physeqs[[1]], measures=c("Shannon"), split=FALSE) # aggregate into one measure
+
+shannon_list <- list()
+for(i in 1:length(physeqs)) {
+  shannon_list[[i]] <- suppressWarnings(estimate_richness(physeqs[[i]], measures=c("Shannon"), split=FALSE)[1,1])
 }
 
+# Remake summary_df with additional columns (Shannon index)
+summary_df <- 
+  data.frame(
+    maxEE = params[,1],
+    truncQ = params[,3],
+    prop.Fs.mapped = unlist(lapply(prop_Fs_mapped_to_asv, function(x) median(unlist(x)))),
+    prop.merged = unlist(lapply(prop_merged, function(x) median(x))),
+    rate.merged = unlist(lapply(merged_variants_per_input_read, function(x) median(x))),
+    abund.spp.classification = unlist(abund_spp_classification),
+    n.spp.classification = unlist(n_spp_classification),
+    shannon.div = unlist(shannon_list)
+  )
+summary_df %>%
+  ggplot(aes(x=truncQ, y=shannon.div, col=maxEE)) +
+  geom_point(alpha=0.6) +
+  scale_colour_gradient(low="blue", high="red") +
+  ggtitle("Shannon diversity of aggregated samples") +
+  xlab("truncQ: quality score at which point to truncate read") +
+  ylab("Shannon diversity (H)")
